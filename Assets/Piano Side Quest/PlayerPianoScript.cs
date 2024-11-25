6172c68451035b7b2b7abd1b0b12ca31673cba79
RushabhGajab2605 <rushabhgajab.official@gmail.com>
2024-11-24 21:21:27 -0700

    Piano Quest Rushabh

--- Assets/Piano Side Quest/PlayerPianoScript.cs
+++ Assets/Piano Side Quest/PlayerPianoScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerPianoScript : MonoBehaviour
 {
     public KeyControl piano;
+    public bool isNearPiano = false;
     public GameObject playPiano;
     // Start is called before the first frame update
     void Start()
@@ -15,15 +16,38 @@ public class PlayerPianoScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isNearPiano && Input.GetKeyDown(KeyCode.P))
+        {
+            playPiano.SetActive(false);
+            piano.ShowPiano();
+        }
         
     }
 
-    private void OnCollisionEnter(Collision other)
+    public void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Piano"))
         {
-            playPiano.SetActive(true);
-            Debug.Log("Piano Collision");
+                playPiano.SetActive(true);
+                isNearPiano = true;
+                Debug.Log("Piano Collision");
+
         }
+
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Debug.Log("Power Up collided.");
+            Destroy(other.gameObject);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Piano"))
+        {
+                playPiano.SetActive(false);
+                isNearPiano = false;
+        }
+
     }
 }
